7db6b996c901cb031b4b55c67e89bc47c8b2f028
Pratyusha Satish Survase <pratyushaswarnim@gmail.com>
2026-02-05 23:03:35 +0530

    Delete Day 4/Lists directory

--- Day 4/Lists/task.py
+++
@@ -1,6 +0,0 @@
-state_of_India=["Maharashtra","Gujarat","Haryana","Goa"]
-#print(state_of_India[0]) print only one item
-
-state_of_India[1]="Tamil nadu"
-state_of_India.extend(["sikkim","nagaland","Delhi"])
-print(state_of_India)
